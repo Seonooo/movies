d72b5ad62d3ee068b7b9e7a82b4871be7c5981b1
Seonooo <eedd22@naver.com>
2022-06-16 18:54:35 +0900

    0616

---
+++ front/src/components/board/ttt.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="aa()">aaaaaaaaaaaaaaaaaaaa</button>
+    {{ state.count }}
+    <hr />
+    {{ state.count.logged }}
+  </div>
+</template>
+
+<script>
+import { onMounted, reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: {},
+    });
+
+    function aa() {
+      state.count = localStorage.getItem("vuex");
+    }
+    onMounted(() => {
+      state.count = JSON.parse(localStorage.getItem("vuex"));
+    });
+
+    return { state, aa };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ front/src/components/homemove.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
+export default {
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+    const state = reactive({
+      count: 0,
+    });
+    store.commit("setLogged", false);
+    router.push("/");
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
